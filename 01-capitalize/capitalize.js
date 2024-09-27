9bae1d9b8b5fb45c3ed5a890d9d5a3e644f4660d
estellel-github <131867732+estellel-github@users.noreply.github.com>
2024-09-27 14:39:19 +0200

    Add tests for 01-capitalize

--- 01-capitalize/capitalize.js
+++ 01-capitalize/capitalize.js
@@ -2,9 +2,14 @@ const capitalize = function (string) {
   if (!string) {
     throw new Error("Please provide a non-empty string!");
   }
-  return string.charAt(0).toUpperCase() + string.slice(1);
+  let newString = string.trim();
+  let firstChar = newString.charAt(0);
+  if (firstChar.toUpperCase() != firstChar.toLowerCase()) {
+    return newString.charAt(0).toUpperCase() + newString.slice(1);
+  }
+  return newString;
 }
 
-console.log(capitalize("this is a test string"));
+console.log(capitalize("0"));
 
 module.exports = capitalize;
--- 01-capitalize/capitalize.spec.js
+++ 01-capitalize/capitalize.spec.js
@@ -1,15 +1,37 @@
 const capitalize = require("./capitalize");
 
 describe("capitalize", function () {
-  test("returns the string with 1st letter capitalized", function () {
-    expect(capitalize("this is a test string")).toEqual(
-      "This is a test string"
+  test("Turns lower-case first char to upper-case", function () {
+    expect(capitalize("test")).toEqual("Test");
+  });
+  test("Maintains upper-case first char", function () {
+    expect(capitalize("Test")).toEqual("Test");
+  });
+  test("Ignores first char if not letter", function () {
+    expect(capitalize("123Test")).toEqual("123Test");
+  });
+  test("Maintains case on rest of string", function () {
+    expect(capitalize("tEsT")).toEqual("TEsT");
+  });
+  test("Works with multiple words", function () {
+    expect(capitalize("test with multiple words")).toEqual(
+      "Test with multiple words"
     );
   });
-  test("throws error if string is empty", function () {
-    expect(() => {
-      capitalize("");
-    }).toThrow("Please provide a non-empty string!");
+  test("Works on single character string", function () {
+    expect(capitalize("t")).toEqual("T");
   });
-
+  test("Removes leading spaces", function () {
+    expect(capitalize("   test with leading space")).toEqual(
+      "Test with leading space"
+    );
+  });
+  test("throws error if input is non-truthy", function () {
+    const nonTruthyValues = [null, undefined, "", 0, NaN, false];
+    nonTruthyValues.forEach((value) => {
+      expect(() => {
+        capitalize(value);
+      }).toThrow("Please provide a non-empty string!");
+    });
+  });
 });
